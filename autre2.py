e4891f45c3df338610bc24a420247ddd73c64f21
Vekorax <46210805+Vekorax@users.noreply.github.com>
2022-11-02 20:18:09 -0400

    Réusinages

--- autre2.py
+++ autre2.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
+"""
+Autre script pour appeler les fonctions
+du script principal, version 2.
+
+Par: Fabrice Morin
+"""
+
 from main import bonjour
 from main import compter
 
 
 def main():
+    """
+    Fonction principale du script
+    """
     bonjour('Morin F.')
     compter(3)
 
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
+"""
+Script principal.
+
+Par: Fabrice Morin
+"""
 import time
 
 
 def bonjour(name):
+    """
+    Pour Saluer
+    :param name: Nom de la personne à saluer.
+    """
     print(f'Bonjour, {name}')
 
 
 def compter(nb, secs=0.25):
+    """
+    Pour compter progressivement.
+    :param nb: jusqu'ou compter
+    :param secs: intervalle de temps entre les nombres
+    """
     print(f"Je sais compter jusqu'à {nb}:", end=' ', flush=True)
     for i in range(1, nb + 1):
         time.sleep(secs)
@@ -16,6 +30,9 @@ def compter(nb, secs=0.25):
 
 
 def main():
+    """
+    Fonction principale du script
+    """
     bonjour('Fabrice M.')
     compter(5)
     compter(10)
